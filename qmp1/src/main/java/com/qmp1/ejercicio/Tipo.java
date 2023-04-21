f1796244a48c726f00cd11ae8d0efa89c7c3cefa
carrilloFN <fcarrillo@frba.utn.edu.ar>
2023-04-21 19:41:21 -0300

    Correccion diagrama de clases

--- qmp1/src/main/java/com/qmp1/ejercicio/Tipo.java
+++ qmp1/src/main/java/com/qmp1/ejercicio/Tipo.java
@@ -2,9 +2,9 @@ package com.qmp1.ejercicio;
 
 public class Tipo {
 
-    public enum TipoPrenda { ZAPATOS, CAMISA_MANGAS_CORTAS, CAMISA_MANGAS_LARGAS, PANTALON }
+    public static enum TipoPrenda { ZAPATOS, CAMISA_MANGAS_CORTAS, CAMISA_MANGAS_LARGAS, PANTALON }
 
-    public enum Categoria{ PARTE_SUPERIOR, CALZADO, PARTE_INFERIOR, ACCESORIOS }
+    public static enum Categoria{ PARTE_SUPERIOR, CALZADO, PARTE_INFERIOR, ACCESORIOS }
 
     private TipoPrenda tipoPrenda;
     private Categoria categoria;
